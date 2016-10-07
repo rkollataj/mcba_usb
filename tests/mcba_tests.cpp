64319f334c78ad8acc872a8d2340567e794a1651
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2016-10-07 14:12:10 +0200

    Tests finished
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- tests/mcba_tests.cpp
+++ tests/mcba_tests.cpp
@@ -81,8 +81,8 @@ int readCAN(int canFd, struct can_frame *frame)
     FD_ZERO(&rfds);
     FD_SET(canFd, &rfds);
 
-    tv.tv_sec = 1;
-    tv.tv_usec = 0;
+    tv.tv_sec = 0;
+    tv.tv_usec = 100000;
     retval = select(canFd+1, &rfds, NULL, NULL, &tv);
 
     EXPECT_GT(retval, -1);
@@ -144,6 +144,553 @@ void setTermination(const char *interface, char val)
     EXPECT_EQ(1, fwrite(&val, 1, 1, f));
 
     fclose(f);
+}
+
+int canReadThread(const char* ifname)
+{
+    int canFd = 0;
+    can_frame frame;
+    int retVal;
+    canid_t i = 0;
+
+//    configureCAN(ifname, 1000000);
+    canFd = openCANSocket(ifname);
+
+    do
+    {
+    retVal = readCAN(canFd, &frame);
+
+    if(retVal)
+    {
+        EXPECT_EQ(i++, frame.can_id);
+    }
+    }
+    while(retVal);
+
+    close(canFd);
+
+    return i;
+}
+
+int canReadThreadNoId(const char* ifname)
+{
+    int canFd = 0;
+    can_frame frame;
+    int retVal;
+    canid_t i = 0;
+
+//    configureCAN(ifname, 1000000);
+    canFd = openCANSocket(ifname);
+
+    do
+    {
+        retVal = readCAN(canFd, &frame);
+
+        EXPECT_GE(retVal, 0);
+
+        i++;
+    }
+    while(retVal);
+
+    close(canFd);
+
+    return i;
+}
+
+int canWriteThread(const char* ifname, const int cnt)
+{
+    int canFd = 0;
+    canid_t i = 0;
+    int dataSent = 0;
+
+//    configureCAN(ifname, 1000000);
+    canFd = openCANSocket(ifname);
+
+    for(i = 0; i <= cnt; ++i)
+    {
+    dataSent = writeCAN(canFd, i, 8, 1, 2, 3, 4, 5, 6, 7, 8);
+
+    usleep(1500);
+
+    EXPECT_EQ(dataSent, CAN_MTU);
+    }
+
+    close(canFd);
+
+    return i;
+}
+
+int canReadThreadExt(const char* ifname, canid_t start, canid_t shift)
+{
+    int canFd = 0;
+    can_frame frame;
+    canid_t i = start;
+    canid_t shifted = 0;
+    int retVal;
+
+//    configureCAN(ifname, 1000000);
+    canFd = openCANSocket(ifname);
+
+    do
+    {
+        retVal = readCAN(canFd, &frame);
+
+        if(retVal)
+        {
+            shifted = i++ << shift;
+
+            EXPECT_EQ(shifted | CAN_EFF_FLAG, frame.can_id);
+        }
+
+    } while(retVal);
+
+    close(canFd);
+
+    return i;
+}
+
+int canWriteThreadExt(const char* ifname, canid_t start, canid_t cnt, canid_t shift)
+{
+    int canFd = 0;
+    canid_t i = 0;
+    canid_t shifted = 0;
+    int dataSent = 0;
+
+//    configureCAN(ifname, 1000000);
+    canFd = openCANSocket(ifname);
+
+    for(i = start; i <= cnt; ++i)
+    {
+        shifted = i << shift;
+
+        dataSent = writeCAN(canFd, shifted | CAN_EFF_FLAG, 8, 1, 2, 3, 4, 5, 6, 7, 8);
+
+        usleep(500);
+
+        EXPECT_EQ(dataSent, CAN_MTU);
+    }
+
+    close(canFd);
+
+    return i;
+}
+
+/***********************************************
+ *
+ *  can0 - Microchip CAN BUS Analyzer
+ *  can1 - Other SocketCAN
+ *
+ ***********************************************/
+
+TEST(can_id_rcv, sid)
+{
+    const int testCnt = 0x7ff;
+
+    std::future<int> readRet = std::async(&canReadThread, "can0");
+    std::future<int> writeRet = std::async(&canWriteThread, "can1", testCnt);
+
+    EXPECT_EQ(testCnt+1, writeRet.get());
+    EXPECT_EQ(testCnt+1, readRet.get());
+}
+
+TEST(can_id_rcv, eid_sid3_sid10)
+{
+    const canid_t start = 1;
+    const canid_t cnt = 0xff;
+    const canid_t shift = MCBA_CAN_E_SID3_SID10_SHIFT;
+
+    std::future<int> readRet = std::async(&canReadThreadExt, "can0", start, shift);
+    std::future<int> writeRet = std::async(&canWriteThreadExt, "can1", start, cnt, shift);
+
+    EXPECT_EQ(cnt+1, writeRet.get());
+    EXPECT_EQ(cnt+1, readRet.get());
+}
+
+TEST(can_id_rcv, eid_sid0_sid2)
+{
+    const canid_t start = 1;
+    const canid_t cnt = 7;
+    const canid_t shift = MCBA_CAN_E_SID0_SID2_SHIFT;
+
+    std::future<int> readRet = std::async(&canReadThreadExt, "can0", start, shift);
+    std::future<int> writeRet = std::async(&canWriteThreadExt, "can1", start, cnt, shift);
+
+    EXPECT_EQ(cnt+1, writeRet.get());
+    EXPECT_EQ(cnt+1, readRet.get());
+}
+
+TEST(can_id_rcv, eid_eid16_eid17)
+{
+    const canid_t start = 1;
+    const canid_t cnt = 3;
+    const canid_t shift = MCBA_CAN_EID16_EID17_SHIFT;
+
+    std::future<int> readRet = std::async(&canReadThreadExt, "can0", start, shift);
+    std::future<int> writeRet = std::async(&canWriteThreadExt, "can1", start, cnt, shift);
+
+    EXPECT_EQ(cnt+1, writeRet.get());
+    EXPECT_EQ(cnt+1, readRet.get());
+}
+
+TEST(can_id_rcv, eid_eid8_eid15)
+{
+    const canid_t start = 1;
+    const canid_t cnt = 0xff;
+    const canid_t shift = MCBA_CAN_EID8_EID15_SHIFT;
+
+    std::future<int> readRet = std::async(&canReadThreadExt, "can0", start, shift);
+    std::future<int> writeRet = std::async(&canWriteThreadExt, "can1", start, cnt, shift);
+
+    EXPECT_EQ(cnt+1, writeRet.get());
+    EXPECT_EQ(cnt+1, readRet.get());
+}
+
+TEST(can_id_rcv, eid_eid0_eid7)
+{
+    const canid_t start = 0;
+    const canid_t cnt = 0xff;
+    const canid_t shift = 0;
+
+    std::future<int> readRet = std::async(&canReadThreadExt, "can0", start, shift);
+    std::future<int> writeRet = std::async(&canWriteThreadExt, "can1", start, cnt, shift);
+
+    EXPECT_EQ(cnt+1, writeRet.get());
+    EXPECT_EQ(cnt+1, readRet.get());
+}
+
+TEST(can_id_snd, sid)
+{
+    const int testCnt = 0x7ff;
+
+    std::future<int> readRet = std::async(&canReadThread, "can1");
+    std::future<int> writeRet = std::async(&canWriteThread, "can0", testCnt);
+
+    EXPECT_EQ(testCnt+1, writeRet.get());
+    EXPECT_EQ(testCnt+1, readRet.get());
+}
+
+
+TEST(can_id_snd, eid_sid3_sid10)
+{
+    const canid_t start = 1;
+    const canid_t cnt = 0xff;
+    const canid_t shift = MCBA_CAN_E_SID3_SID10_SHIFT;
+
+    std::future<int> readRet = std::async(&canReadThreadExt, "can1", start, shift);
+    std::future<int> writeRet = std::async(&canWriteThreadExt, "can0", start, cnt, shift);
+
+    EXPECT_EQ(cnt+1, writeRet.get());
+    EXPECT_EQ(cnt+1, readRet.get());
+}
+
+TEST(can_id_snd, eid_sid0_sid2)
+{
+    const canid_t start = 1;
+    const canid_t cnt = 7;
+    const canid_t shift = MCBA_CAN_E_SID0_SID2_SHIFT;
+
+    std::future<int> readRet = std::async(&canReadThreadExt, "can1", start, shift);
+    std::future<int> writeRet = std::async(&canWriteThreadExt, "can0", start, cnt, shift);
+
+    EXPECT_EQ(cnt+1, writeRet.get());
+    EXPECT_EQ(cnt+1, readRet.get());
+}
+
+TEST(can_id_snd, eid_eid16_eid17)
+{
+    const canid_t start = 1;
+    const canid_t cnt = 3;
+    const canid_t shift = MCBA_CAN_EID16_EID17_SHIFT;
+
+    std::future<int> readRet = std::async(&canReadThreadExt, "can1", start, shift);
+    std::future<int> writeRet = std::async(&canWriteThreadExt, "can0", start, cnt, shift);
+
+    EXPECT_EQ(cnt+1, writeRet.get());
+    EXPECT_EQ(cnt+1, readRet.get());
+}
+
+TEST(can_id_snd, eid_eid8_eid15)
+{
+    const canid_t start = 1;
+    const canid_t cnt = 0xff;
+    const canid_t shift = MCBA_CAN_EID8_EID15_SHIFT;
+
+    std::future<int> readRet = std::async(&canReadThreadExt, "can1", start, shift);
+    std::future<int> writeRet = std::async(&canWriteThreadExt, "can0", start, cnt, shift);
+
+    EXPECT_EQ(cnt+1, writeRet.get());
+    EXPECT_EQ(cnt+1, readRet.get());
+}
+
+TEST(can_id_snd, eid_eid0_eid7)
+{
+    const canid_t start = 0;
+    const canid_t cnt = 0xff;
+    const canid_t shift = 0;
+
+    std::future<int> readRet = std::async(&canReadThreadExt, "can1", start, shift);
+    std::future<int> writeRet = std::async(&canWriteThreadExt, "can0", start, cnt, shift);
+
+    EXPECT_EQ(cnt+1, writeRet.get());
+    EXPECT_EQ(cnt+1, readRet.get());
+}
+
+TEST(stress_snd, 1on1)
+{
+    const int testCnt = 0x7ff;
+
+    std::future<int> readRet = std::async(&canReadThread, "can1");
+    std::future<int> writeRet = std::async(&canWriteThread, "can0", testCnt);
+
+    EXPECT_EQ(testCnt+1, writeRet.get());
+    EXPECT_EQ(testCnt+1, readRet.get());
+}
+
+TEST(stress_snd, 1on2)
+{
+    const int testCnt = 0x7ff;
+
+    std::future<int> readRet1 = std::async(&canReadThread, "can1");
+    std::future<int> readRet2 = std::async(&canReadThread, "can1");
+    std::future<int> writeRet = std::async(&canWriteThread, "can0", testCnt);
+
+    EXPECT_EQ(testCnt+1, writeRet.get());
+    EXPECT_EQ(testCnt+1, readRet1.get());
+    EXPECT_EQ(testCnt+1, readRet2.get());
+}
+
+TEST(stress_snd, 1on4)
+{
+    const int testCnt = 0x7ff;
+
+    std::future<int> readRet1 = std::async(&canReadThread, "can1");
+    std::future<int> readRet2 = std::async(&canReadThread, "can1");
+    std::future<int> readRet3 = std::async(&canReadThread, "can1");
+    std::future<int> readRet4 = std::async(&canReadThread, "can1");
+    std::future<int> writeRet = std::async(&canWriteThread, "can0", testCnt);
+
+    EXPECT_EQ(testCnt+1, writeRet.get());
+    EXPECT_EQ(testCnt+1, readRet1.get());
+    EXPECT_EQ(testCnt+1, readRet2.get());
+    EXPECT_EQ(testCnt+1, readRet3.get());
+    EXPECT_EQ(testCnt+1, readRet4.get());
+}
+
+TEST(stress_snd, 2on1)
+{
+    const int testCnt = 0x7ff;
+
+    std::future<int> readRet = std::async(&canReadThreadNoId, "can1");
+    std::future<int> writeRet1 = std::async(&canWriteThread, "can0", testCnt);
+    std::future<int> writeRet2 = std::async(&canWriteThread, "can0", testCnt);
+
+    EXPECT_EQ(testCnt+1, writeRet1.get());
+    EXPECT_EQ(testCnt+1, writeRet2.get());
+    EXPECT_EQ((testCnt+1)*2+1, readRet.get());
+}
+
+TEST(stress_snd, 4on1)
+{
+    const int testCnt = 0x7ff;
+
+    std::future<int> readRet = std::async(&canReadThreadNoId, "can1");
+    std::future<int> writeRet1 = std::async(&canWriteThread, "can0", testCnt);
+    std::future<int> writeRet2 = std::async(&canWriteThread, "can0", testCnt);
+    std::future<int> writeRet3 = std::async(&canWriteThread, "can0", testCnt);
+    std::future<int> writeRet4 = std::async(&canWriteThread, "can0", testCnt);
+
+    EXPECT_EQ(testCnt+1, writeRet1.get());
+    EXPECT_EQ(testCnt+1, writeRet2.get());
+    EXPECT_EQ(testCnt+1, writeRet3.get());
+    EXPECT_EQ(testCnt+1, writeRet4.get());
+    EXPECT_EQ((testCnt+1)*4+1, readRet.get());
+}
+
+TEST(stress_rcv, 1on1)
+{
+    const int testCnt = 0x7ff;
+
+    std::future<int> readRet = std::async(&canReadThread, "can0");
+    std::future<int> writeRet = std::async(&canWriteThread, "can1", testCnt);
+
+    EXPECT_EQ(testCnt+1, writeRet.get());
+    EXPECT_EQ(testCnt+1, readRet.get());
+}
+
+TEST(stress_rcv, 1on2)
+{
+    const int testCnt = 0x7ff;
+
+    std::future<int> readRet1 = std::async(&canReadThread, "can0");
+    std::future<int> readRet2 = std::async(&canReadThread, "can0");
+    std::future<int> writeRet = std::async(&canWriteThread, "can1", testCnt);
+
+    EXPECT_EQ(testCnt+1, writeRet.get());
+    EXPECT_EQ(testCnt+1, readRet1.get());
+    EXPECT_EQ(testCnt+1, readRet2.get());
+}
+
+TEST(stress_rcv, 1on4)
+{
+    const int testCnt = 0x7ff;
+
+    std::future<int> readRet1 = std::async(&canReadThread, "can0");
+    std::future<int> readRet2 = std::async(&canReadThread, "can0");
+    std::future<int> readRet3 = std::async(&canReadThread, "can0");
+    std::future<int> readRet4 = std::async(&canReadThread, "can0");
+    std::future<int> writeRet = std::async(&canWriteThread, "can1", testCnt);
+
+    EXPECT_EQ(testCnt+1, writeRet.get());
+    EXPECT_EQ(testCnt+1, readRet1.get());
+    EXPECT_EQ(testCnt+1, readRet2.get());
+    EXPECT_EQ(testCnt+1, readRet3.get());
+    EXPECT_EQ(testCnt+1, readRet4.get());
+}
+
+TEST(stress_rcv, 2on1)
+{
+    const int testCnt = 0x7ff;
+
+    std::future<int> readRet = std::async(&canReadThreadNoId, "can0");
+    std::future<int> writeRet1 = std::async(&canWriteThread, "can1", testCnt);
+    std::future<int> writeRet2 = std::async(&canWriteThread, "can1", testCnt);
+
+    EXPECT_EQ(testCnt+1, writeRet1.get());
+    EXPECT_EQ(testCnt+1, writeRet2.get());
+    EXPECT_EQ((testCnt+1)*2+1, readRet.get());
+}
+
+TEST(stress_rcv, 4on1)
+{
+    const int testCnt = 0x7ff;
+
+    std::future<int> readRet = std::async(&canReadThreadNoId, "can0");
+    std::future<int> writeRet1 = std::async(&canWriteThread, "can1", testCnt);
+    std::future<int> writeRet2 = std::async(&canWriteThread, "can1", testCnt);
+    std::future<int> writeRet3 = std::async(&canWriteThread, "can1", testCnt);
+    std::future<int> writeRet4 = std::async(&canWriteThread, "can1", testCnt);
+
+    EXPECT_EQ(testCnt+1, writeRet1.get());
+    EXPECT_EQ(testCnt+1, writeRet2.get());
+    EXPECT_EQ(testCnt+1, writeRet3.get());
+    EXPECT_EQ(testCnt+1, writeRet4.get());
+    EXPECT_EQ((testCnt+1)*4+1, readRet.get());
+}
+
+TEST(dlc_snd, dlc)
+{
+    int dataSent = 0;
+    can_frame frame;
+    int dataRcv;
+
+    int canFdSnd = openCANSocket("can0");
+    int canFdRcv = openCANSocket("can1");
+
+    for(u8 i = 0; i <= 8; ++i)
+    {
+        dataSent = writeCAN(canFdSnd, 0xff, i, 1, 2, 3, 4, 5, 6, 7, 8);
+
+        EXPECT_EQ(dataSent, CAN_MTU);
+
+        dataRcv = readCAN(canFdRcv, &frame);
+
+        EXPECT_GE(dataRcv, CAN_MTU);
+        EXPECT_EQ(frame.can_dlc, i);
+    }
+
+    close(canFdSnd);
+    close(canFdRcv);
+}
+
+TEST(dlc_rcv, dlc)
+{
+    int dataSent = 0;
+    can_frame frame;
+    int dataRcv;
+
+    int canFdSnd = openCANSocket("can1");
+    int canFdRcv = openCANSocket("can0");
+
+    for(u8 i = 0; i <= 8; ++i)
+    {
+        dataSent = writeCAN(canFdSnd, 0xff, i, 1, 2, 3, 4, 5, 6, 7, 8);
+
+        EXPECT_EQ(dataSent, CAN_MTU);
+
+        dataRcv = readCAN(canFdRcv, &frame);
+
+        EXPECT_GE(dataRcv, CAN_MTU);
+        EXPECT_EQ(frame.can_dlc, i);
+    }
+
+    close(canFdSnd);
+    close(canFdRcv);
+}
+
+TEST(data_snd, data)
+{
+    int dataSent = 0;
+    can_frame frame;
+    int dataRcv;
+    u8 data[] = {0, 1, 2, 3, 4, 5, 6, 7};
+
+    int canFdSnd = openCANSocket("can0");
+    int canFdRcv = openCANSocket("can1");
+
+    for(u32 i = 0; i <= 255; ++i)
+    {
+        dataSent = writeCAN(canFdSnd, 0xff, 8, data[0] + i, data[1] + i, data[2] + i, data[3] + i, data[4] + i, data[5] + i, data[6] + i, data[7] + i);
+
+        EXPECT_EQ(dataSent, CAN_MTU);
+
+        dataRcv = readCAN(canFdRcv, &frame);
+
+        EXPECT_GE(dataRcv, CAN_MTU);
+        EXPECT_EQ(frame.data[0], (u8)(data[0] + i));
+        EXPECT_EQ(frame.data[1], (u8)(data[1] + i));
+        EXPECT_EQ(frame.data[2], (u8)(data[2] + i));
+        EXPECT_EQ(frame.data[3], (u8)(data[3] + i));
+        EXPECT_EQ(frame.data[4], (u8)(data[4] + i));
+        EXPECT_EQ(frame.data[5], (u8)(data[5] + i));
+        EXPECT_EQ(frame.data[6], (u8)(data[6] + i));
+        EXPECT_EQ(frame.data[7], (u8)(data[7] + i));
+    }
+
+    close(canFdSnd);
+    close(canFdRcv);
+}
+
+TEST(data_rcv, data)
+{
+    int dataSent = 0;
+    can_frame frame;
+    int dataRcv;
+    u8 data[] = {0, 1, 2, 3, 4, 5, 6, 7};
+
+    int canFdSnd = openCANSocket("can1");
+    int canFdRcv = openCANSocket("can0");
+
+    for(u32 i = 0; i <= 255; ++i)
+    {
+        dataSent = writeCAN(canFdSnd, 0xff, 8, data[0] + i, data[1] + i, data[2] + i, data[3] + i, data[4] + i, data[5] + i, data[6] + i, data[7] + i);
+
+        EXPECT_EQ(dataSent, CAN_MTU);
+
+        dataRcv = readCAN(canFdRcv, &frame);
+
+        EXPECT_GE(dataRcv, CAN_MTU);
+        EXPECT_EQ(frame.data[0], (u8)(data[0] + i));
+        EXPECT_EQ(frame.data[1], (u8)(data[1] + i));
+        EXPECT_EQ(frame.data[2], (u8)(data[2] + i));
+        EXPECT_EQ(frame.data[3], (u8)(data[3] + i));
+        EXPECT_EQ(frame.data[4], (u8)(data[4] + i));
+        EXPECT_EQ(frame.data[5], (u8)(data[5] + i));
+        EXPECT_EQ(frame.data[6], (u8)(data[6] + i));
+        EXPECT_EQ(frame.data[7], (u8)(data[7] + i));
+    }
+
+    close(canFdSnd);
+    close(canFdRcv);
 }
 
 TEST(canIDConvertion, standardId)
@@ -318,76 +865,3 @@ TEST(Configuration, Termination)
     value = getTermination("can0");
     EXPECT_EQ(initValue, value);
 }
-
-
-//int canReadThread(const char* ifname)
-//{
-//    int canFd = 0;
-//    can_frame frame;
-//    int retVal;
-//    int i = 0;
-
-////    configureCAN(ifname, 1000000);
-//    canFd = openCANSocket(ifname);
-
-//    do
-//    {
-//        retVal = readCAN(canFd, &frame);
-
-//        if(retVal)
-//        {
-//            ++i;
-//        }
-//    }
-//    while(retVal);
-
-//    close(canFd);
-
-//    return i;
-//}
-
-//int canWriteThread(const char* ifname, const int cnt)
-//{
-//    int canFd = 0;
-//    int i = 0;
-//    int dataSent = 0;
-
-////    configureCAN(ifname, 1000000);
-//    canFd = openCANSocket(ifname);
-
-//    for(i = 1; i <= cnt; ++i)
-//    {
-//        dataSent = writeCAN(canFd, i, 8, 1, 2, 3, 4, 5, 6, 7, 8);
-
-//        usleep(10000);
-
-//        EXPECT_EQ(dataSent, CAN_MTU);
-//    }
-
-//    close(canFd);
-
-//    return i-1;
-//}
-
-//TEST(StressTest, 1on1)
-//{
-//    int can0_term = -1;
-//    int can1_term = -1;
-//    const int testCnt = 1000;
-
-//    can0_term = getTermination("can0");
-//    can1_term = getTermination("can1");
-
-//    setTermination("can0", '1');
-//    setTermination("can1", '0');
-
-//    std::future<int> readRet = std::async(&canReadThread, "can0");
-//    std::future<int> writeRet = std::async(&canWriteThread, "can1", testCnt);
-
-//    EXPECT_EQ(testCnt, writeRet.get());
-//    EXPECT_EQ(testCnt, readRet.get());
-
-//    //bring back original termination
-//    setTermination("can0", can0_term);
-//    setTermination("can1", can1_term);
-//}
