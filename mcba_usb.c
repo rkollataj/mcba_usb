07f6560c565cc4229feacfbbd930813f6c4ffc8c
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2017-01-13 09:24:13 +0100

    calls to led functions removed
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- mcba_usb.c
+++ mcba_usb.c
@@ -951,8 +951,6 @@ static int mcba_usb_open(struct net_device *netdev)
 	if (err)
 		return err;
 
-	can_led_event(netdev, CAN_LED_EVENT_OPEN);
-
 	netif_start_queue(netdev);
 
 	return 0;
@@ -977,8 +975,6 @@ static int mcba_usb_close(struct net_device *netdev)
 	mcba_urb_unlink(priv);
 
 	close_candev(netdev);
-
-	can_led_event(netdev, CAN_LED_EVENT_STOP);
 
 	return 0;
 }
